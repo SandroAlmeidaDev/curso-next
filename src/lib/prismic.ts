2b8a3e429309f16f64a00db7a8cd258193d4a387
SandroAlmeidaDev <sandroalmeida25@hotmail.com>
2020-11-09 22:03:57 -0300

    Add CMS prismic

---
+++ src/lib/prismic.ts
@@ -0,0 +1,9 @@
+import Prismic from 'prismic-javascript'
+
+export const apiEndpoint = process.env.ENDPOINT_API_PRISMIC
+
+export const client = (req = null) => {
+  const options = req ? { req } : null
+
+  return Prismic.client(apiEndpoint, options)
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,51 @@
+import { GetServerSideProps } from 'next'
+import Link from 'next/link'
+
+import { Title } from '@/styles/pages/Home'
+import SEO from '@/components/SEO'
+import { client } from '@/lib/prismic'
+import Prismic from 'prismic-javascript'
+import PrismicDOM from 'prismic-dom'
+import { Document } from 'prismic-javascript/types/documents'
+import { useRouter } from 'next/router'
+
+interface HomeProps {
+  recommendedProducts: Document[]
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
+  const router = useRouter()
+
+  return (
+    <div>
+      <SEO
+        title="DevCommerce | boosting your dev things"
+        image="boost.png"
+        shouldExcludeTitleSuffix
+      />
+      <section>
+        <Title>Products</Title>
+        <span onClick={() => router.push('search')}>Buscar</span>
+        <ul>
+          {recommendedProducts.map(product => (
+            <li key={product.id}>
+              <Link href={`catalog/products/${product.uid}`}>
+                <a>{PrismicDOM.RichText.asText(product.data.title)}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at('document.type', 'product')
+  ])
+
+  return {
+    props: { recommendedProducts: recommendedProducts.results }
+  }
+}
